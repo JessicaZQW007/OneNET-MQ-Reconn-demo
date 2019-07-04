ac160103270ac3bea5acd21d310ee67cf4c20674
10713 <1071306022@qq.com>
2019-07-04 13:36:54 +0800

    Initial commit

---
+++ src/main/java/com/onenet/mq/decodeMessage.java
@@ -0,0 +1,12 @@
+package com.onenet.mq;
+
+/**
+ * @Author: 张权威
+ * @Date: 2019/6/28 10:14
+ */
+public class decodeMessage {
+    public static  String  decodeMessage(String body){
+        System.out.println(body);
+        return "";
+    }
+}
